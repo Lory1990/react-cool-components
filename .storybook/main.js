6bdd5566afdb9c65ea6e4f1a0ae3fc03af02ff74
Lorenzo <lory1990@gmail.com>
2021-07-03 16:48:55 +0200

    fix public pth

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,13 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.output.publicPath = '/react-cool-components/';
+    return config;
+  },
+  managerWebpack: async (config) => {
+    config.output.publicPath = '/react-cool-components/';
+    return config;
+  },
 }
