b4e0736ca49153956d31b2f8f990225a55c58101
Giannascoli Gloria <giannascoli.gloria@gmail.com>
2021-07-03 16:52:57 +0200

    wip checkbox

--- src/inputs/CheckBox/CheckBox.stories.tsx
+++ src/inputs/CheckBox/CheckBox.stories.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
 
+import { Meta } from '@storybook/react';
+import CheckBox, { ICheckBoxProps } from './CheckBox';
 
-export default function CheckBox(){
-    
-}
+export default {
+    title: "Components/CheckBox",
+    component: CheckBox,
+} as Meta
+
+// Button.stories.js
+
+//👇 We create a “template” of how args map to rendering
+export const Template = (args : ICheckBoxProps) => <CheckBox {...args} />;
+
+//👇 Each story then reuses that template
+//export const Primary = Template.bind({});
+Template.args = { background: '#ff0', label: 'CheckBox' };
--- src/inputs/CheckBox/CheckBox.tsx
+++ src/inputs/CheckBox/CheckBox.tsx
@@ -1,5 +1,41 @@
+import './CheckBox.module.scss'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Tooltip from '@material-ui/core/Tooltip'
+import classnames from  'classnames'
+import _ from 'lodash';
 
+export interface ICheckBoxProps{
+    color: string,
+    onChange?:(event: any) => void,
+    id?: string,
+    disabled:boolean | undefined,
+    label: string,
+    name: string,
+    value: string,
+    className: string,
+    tooltip: string
+}
 
-export default function CheckBox(){
-    
+export default function  CheckboxComponent({onChange, id, disabled, label, name, value, className, tooltip} : ICheckBoxProps){
+
+    return(<FormControlLabel 
+        className={classnames('checkbox',className, {disabled})}
+        control={
+            <>
+            <Checkbox
+            checked={Boolean(value)}
+            onChange={onChange}
+            id={id}
+            name={name}
+            disabled={disabled}
+            />
+            </>
+        }            
+        disabled={disabled}
+        label={<>
+            {label}
+        </>}
+        />
+    )
 }
