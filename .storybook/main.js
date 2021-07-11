7ef131f179bfb416a7c363f5dc965ed89730fdf2
De Francesco Lorenzo <lory1990@gmail.com>
2021-07-11 14:30:40 +0200

    FIgma integration

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,59 +3,46 @@ const tsconfig = path.resolve(__dirname, "../tsconfig.json")
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "storybook-vscode-component/register",
-  ],
-  typescript: {
-    check: true,
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      tsconfigPath: tsconfig,
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    }
-  },
-  webpackFinal: async (config, { configType }) => {
-
-    config.resolve.plugins.push(new TsconfigPathsPlugin())
-
-    
-    config.module.rules.push({
-      test: /\.s[ac]ss$/i,
-      use: [
-        // 'style-loader', 
-        // 'css-loader',
-        'sass-loader',
-        { 
-          loader: 'sass-resources-loader',
-          options:{            
-            resources: [
-              path.resolve(__dirname, "../src/style/index.scss"),
-            ]
-          },
+    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "storybook-vscode-component/register", "storybook-addon-designs"],
+    typescript: {
+        check: true,
+        reactDocgen: "react-docgen-typescript",
+        reactDocgenTypescriptOptions: {
+            tsconfigPath: tsconfig,
+            compilerOptions: {
+                allowSyntheticDefaultImports: false,
+                esModuleInterop: false
+            }
         }
-      ],
-      
-    });
+    },
+    webpackFinal: async (config, { configType }) => {
+        config.resolve.plugins.push(new TsconfigPathsPlugin())
 
-    if(process.env.NODE_ENV === 'production'){
-      config.output.publicPath = '/react-cool-components/';
+        config.module.rules.push({
+            test: /\.s[ac]ss$/i,
+            use: [
+                // 'style-loader',
+                // 'css-loader',
+                "sass-loader",
+                {
+                    loader: "sass-resources-loader",
+                    options: {
+                        resources: [path.resolve(__dirname, "../src/style/index.scss")]
+                    }
+                }
+            ]
+        })
+
+        if (process.env.NODE_ENV === "production") {
+            config.output.publicPath = "/react-cool-components/"
+        }
+        return config
+    },
+    managerWebpack: async config => {
+        if (process.env.NODE_ENV === "production") {
+            config.output.publicPath = "/react-cool-components/"
+        }
+        return config
     }
-    return config;
-  },
-  managerWebpack: async (config) => {
-    if(process.env.NODE_ENV === 'production'){
-      config.output.publicPath = '/react-cool-components/';
-    }
-    return config;
-  },
 }
--- src/cards/SubscriptionCard/SubscriptionCard.stories.tsx
+++ src/cards/SubscriptionCard/SubscriptionCard.stories.tsx
@@ -1,101 +1,105 @@
-import { Meta } from '@storybook/react';
-import SubscriptionCard, { ISubscriptionCardProps, SubscriptionCardTypology } from './SubscriptionCard'
-import { getStoryPath } from 'utils/FileUtils';
-import { baseAbsolute, file, wd } from 'paths.macro';
+import { Meta } from "@storybook/react"
+import SubscriptionCard, { ISubscriptionCardProps, SubscriptionCardTypology } from "./SubscriptionCard"
+import { getStoryPath } from "utils/FileUtils"
+import { baseAbsolute, file, wd } from "paths.macro"
 
 export default {
-    title: 'Cards/SubscriptionCard',
+    title: "Cards/SubscriptionCard",
     component: SubscriptionCard,
     parameters: {
-      storySource: {
-        storyPath:getStoryPath(baseAbsolute, wd, file)
-      },
+        storySource: {
+            storyPath: getStoryPath(baseAbsolute, wd, file)
+        },
+        design: {
+            type: "figma",
+            url: "https://www.figma.com/file/iyH3v3Lgfzi10fpz2UrOf0/Tongy_redesign?node-id=10%3A573"
+        }
     }
-} as Meta;
-  
+} as Meta
+
 export const Standard = (args: ISubscriptionCardProps) => {
-  return <SubscriptionCard {...args}/>;
+    return <SubscriptionCard {...args} />
 }
 
 Standard.args = {
     price: {
-      currency: "€",
-      value: 10
+        currency: "€",
+        value: 10
     },
     title: "Sottoscrizione",
     paymentFrequency: 2,
     mostConvenient: false,
     typology: SubscriptionCardTypology.STANDARD,
     features: [
-      {
-        included: true,
-        text: "Feature 1",
-      },
-      {
-        included: true,
-        text: "Feature 2",
-      },
-      {
-        included: false,
-        text: "Feature 3",
-      }
+        {
+            included: true,
+            text: "Feature 1"
+        },
+        {
+            included: true,
+            text: "Feature 2"
+        },
+        {
+            included: false,
+            text: "Feature 3"
+        }
     ]
-} as ISubscriptionCardProps;
+} as ISubscriptionCardProps
 
 export const Package = (args: ISubscriptionCardProps) => {
-  return <SubscriptionCard {...args}/>;
+    return <SubscriptionCard {...args} />
 }
 
 Package.args = {
     price: {
-      currency: "€",
-      value: 10
+        currency: "€",
+        value: 10
     },
     title: "Sottoscrizione",
     paymentFrequency: 2,
     mostConvenient: false,
     typology: SubscriptionCardTypology.PACKAGE,
     features: [
-      {
-        included: true,
-        text: "Feature 1",
-      },
-      {
-        included: true,
-        text: "Feature 2",
-      },
-      {
-        included: false,
-        text: "Feature 3",
-      }
+        {
+            included: true,
+            text: "Feature 1"
+        },
+        {
+            included: true,
+            text: "Feature 2"
+        },
+        {
+            included: false,
+            text: "Feature 3"
+        }
     ]
-} as ISubscriptionCardProps;
+} as ISubscriptionCardProps
 
 export const MostConvenient = (args: ISubscriptionCardProps) => {
-  return <SubscriptionCard {...args}/>;
+    return <SubscriptionCard {...args} />
 }
 
 MostConvenient.args = {
     price: {
-      currency: "€",
-      value: 10
+        currency: "€",
+        value: 10
     },
     title: "Sottoscrizione",
     paymentFrequency: 2,
     mostConvenient: true,
     typology: SubscriptionCardTypology.STANDARD,
     features: [
-      {
-        included: true,
-        text: "Feature 1",
-      },
-      {
-        included: true,
-        text: "Feature 2",
-      },
-      {
-        included: false,
-        text: "Feature 3",
-      }
+        {
+            included: true,
+            text: "Feature 1"
+        },
+        {
+            included: true,
+            text: "Feature 2"
+        },
+        {
+            included: false,
+            text: "Feature 3"
+        }
     ]
-} as ISubscriptionCardProps;
+} as ISubscriptionCardProps
