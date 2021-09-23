d47a1beef7893536e69f220a2a5e7a26faddaa77
Lorenzo De Francesco <lory1990@gmail.com>
2021-09-23 09:31:37 +0200

    wip

--- src/storybook-css-regression.test.js
+++ src/storybook-css-regression.test.js
@@ -1,11 +1,18 @@
 import initStoryshots from "@storybook/addon-storyshots"
+import path from 'path'
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer"
-// import path from "path"
 
 //https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-puppeteer#customizing-a-page-instance
 
 //https://www.gitmemory.com/issue/storybookjs/storybook/7587/790395185
 const beforeScreenshot = page => page.$("#root > *").then(root => root ?? undefined)
+
+const getGotoOptions = ({context,url})=>{
+    return{
+        waitUntil: "networkidle2"
+    }
+}
+
 
 const getScreenshotOptions = ({ context, url }) => {
     return {
@@ -14,15 +21,16 @@ const getScreenshotOptions = ({ context, url }) => {
     }
 }
 
+
 initStoryshots({
     // asyncJest: true,
     suite: "Storyshots - CSS Regression test",
     test: imageSnapshot({
-        storybookUrl: "http://localhost:6006/",
-        // storybookUrl: `file://${path.resolve(__dirname, "../storybook-static")}`,
+        //storybookUrl: "http://localhost:6006/",
+        storybookUrl: `file://${path.resolve(__dirname, "../storybook-static")}`,
         beforeScreenshot,
-        getScreenshotOptions
-        // getGotoOptions,
+        getScreenshotOptions,
+        getGotoOptions,
     }),
     framework: "react"
 })
