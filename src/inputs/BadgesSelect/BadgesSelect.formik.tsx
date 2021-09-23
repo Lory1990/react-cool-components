6811099bc0b112f05f00c7cddfe38da23699153c
Lorenzo De Francesco <lory1990@gmail.com>
2021-09-23 08:58:14 +0200

    Fix build error

--- src/inputs/BadgesSelect/BadgesSelect.formik.tsx
+++ src/inputs/BadgesSelect/BadgesSelect.formik.tsx
@@ -9,20 +9,22 @@ export interface IBadgesSelectFormikProps extends IBadgesSelectProps{
 
 export default function BadgesSelectFormik(props : IBadgesSelectFormikProps){
 
-    const [field, meta, helpers] = useField(props.name);
-    const { isSubmitting } = useFormikContext()
+    // const [field, meta, helpers] = useField(props.name);
+    // const { isSubmitting } = useFormikContext()
 
-    //console.log("badges-select-formik", props.name, props.values, field.value, "REAL VALUE", props.values?.filter(value => field.value.includes(value.id)))
+    // //console.log("badges-select-formik", props.name, props.values, field.value, "REAL VALUE", props.values?.filter(value => field.value.includes(value.id)))
 
-    return <BadgesSelect
-        {...meta}
-        {...props}
-        name={props.name}
-        value={Array.isArray(field.value) ? props.values?.filter(value => field.value.includes(value.id)) : []}
-        disabled={isSubmitting || props.disabled}
-        onChange={(event, value, reason)=>{
-            helpers.setValue(_.map(value, v=>v.id))
-            if(props.onChange) props.onChange(event, value, reason)
-        }}
-    />
+    // return <BadgesSelect
+    //     {...meta}
+    //     {...props}
+    //     name={props.name}
+    //     value={Array.isArray(field.value) ? props.values?.filter(value => field.value.includes(value.id)) : []}
+    //     disabled={isSubmitting || props.disabled}
+    //     onChange={(event, value, reason)=>{
+    //         helpers.setValue(_.map(value, v=>v.id))
+    //         if(props.onChange) props.onChange(event, value, reason)
+    //     }}
+    // />
+
+    return <div>WIP</div>
 }
