ac9a7653f15ae3684aafbe0d8b37723bf58b22ca
Lorenzo De Francesco <lory1990@gmail.com>
2021-10-03 10:57:07 +0200

    Working on #3

--- src/gallery/TwoElementsGallery/TwoElementsGallery.stories.tsx
+++ src/gallery/TwoElementsGallery/TwoElementsGallery.stories.tsx
@@ -1,16 +1,39 @@
-import { Story } from "@storybook/react"
-import TwoElementsGallery, { TwoElementsGalleryProps } from './TwoElementsGallery'
+import { Story, Meta } from "@storybook/react"
+import TwoElementsGallery, { ITwoElementsGalleryProps } from "./TwoElementsGallery"
+import { getStoryPath } from "@utils/FileUtils"
+import { baseAbsolute, file, wd } from "paths.macro"
 
 export default {
-    title: "TwoElementsGallery",
+    title: "Gallery/TwoElementsGallery",
     component: TwoElementsGallery,
-}
+    parameters: {
+        layout: "centered",
+        storySource: {
+            storyPath: getStoryPath(baseAbsolute, wd, file)
+        }
+    }
+} as Meta
 
-const Template = (args: TwoElementsGalleryProps) => <TwoElementsGallery {...args} />
+const Template = (args: ITwoElementsGalleryProps) => <TwoElementsGallery {...args} />
 
-export const Plain: Story<TwoElementsGalleryProps> = Template.bind({})
+export const Plain: Story<ITwoElementsGalleryProps> = Template.bind({})
+export const OnlyLeftImage: Story<ITwoElementsGalleryProps> = Template.bind({})
+export const OnlyRightImage: Story<ITwoElementsGalleryProps> = Template.bind({})
+export const NoImages: Story<ITwoElementsGalleryProps> = Template.bind({})
 
 Plain.args = {
     imageLeft: "https://images.unsplash.com/photo-1626626925024-aaf056134c20?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
-    imageRight: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-} as TwoElementsGalleryProps
+    imageLeftAlt: "Man",
+    imageRight: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    imageRightAlt: "River"
+} as ITwoElementsGalleryProps
+
+OnlyLeftImage.args = {
+    imageLeft: "https://images.unsplash.com/photo-1626626925024-aaf056134c20?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+    imageLeftAlt: "Man"
+} as ITwoElementsGalleryProps
+
+OnlyRightImage.args = {
+    imageRight: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    imageRightAlt: "River"
+} as ITwoElementsGalleryProps
--- src/gallery/TwoElementsGallery/TwoElementsGallery.tsx
+++ src/gallery/TwoElementsGallery/TwoElementsGallery.tsx
@@ -1,27 +1,48 @@
-import classnames from 'classnames'
+import classnames from "classnames"
 import style from "./TwoElementsGallery.module.scss"
 
-
-export type TwoElementsGalleryProps = {
-    imageLeft: string,
-    imageRight: string
+export interface ITwoElementsGalleryProps {
+    imageLeft?: string
+    imageLeftAlt?: string
+    imageRight?: string
+    imageRightAlt?: string
+    className?: string
+    classes?: {
+        image?: string
+        leftImage?: string
+        rightImage?: string
+    }
 }
 
-export const TwoElementsGallery = ({imageLeft, imageRight} : TwoElementsGalleryProps) => {
+export const TwoElementsGallery = ({ imageLeft, imageRight, imageLeftAlt = "", imageRightAlt = "", className, classes }: ITwoElementsGalleryProps) => {
     return (
-      <div className={classnames("two-elements-gallery-container")}>
-        <img
-          src={imageLeft}
-          alt=""
-          className={classnames("two-elements-gallery__image", "two-elements-gallery__image-left")}
-        />
-        <img
-          src={imageRight}
-          alt=""
-          className={classnames("two-elements-gallery__image", "two-elements-gallery__image-right")}
-        />
-      </div>
-    );
-  };
+        <div className={classnames(style["two-elements-gallery-container"], "two-elements-gallery-container", className)}>
+            <img
+                src={imageLeft}
+                alt={imageLeftAlt}
+                className={classnames(
+                    classes?.image,
+                    classes?.leftImage,
+                    style["two-elements-gallery__image"],
+                    style["two-elements-gallery__image-left"],
+                    "two-elements-gallery__image",
+                    "two-elements-gallery__image-left"
+                )}
+            />
+            <img
+                src={imageRight}
+                alt={imageRightAlt}
+                className={classnames(
+                    classes?.image,
+                    classes?.rightImage,
+                    style["two-elements-gallery__image"],
+                    style["two-elements-gallery__image-right"],
+                    "two-elements-gallery__image",
+                    "two-elements-gallery__image-right"
+                )}
+            />
+        </div>
+    )
+}
 
-  export default TwoElementsGallery
+export default TwoElementsGallery
